de7829065509fa291b2aca71bd24eae7e548c2ea
arily <lbsaaaaa@gmail.com>
2020-06-13 09:29:18 +0900

    add locales

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f2790f82896cd806099b3680deb2476f.js"
+  "precache-manifest.0301e0bd488bc1d986354e3bbd430c3f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sb-spa"});
